32766c2722fb94a52b94c3ec47164c677d3259a8
daust05 <daust05@gmail.com>
2021-02-23 22:13:18 +0900

    (Nodejs) +writeFile

--- nodejs/ex1.js
+++ nodejs/ex1.js
@@ -1,6 +1,7 @@
 let http = require('http');
 let fs = require('fs');
 let url = require('url');
+var qs = require('querystring');
 
 function errorOnPage(message, response){
     console.log(message);
@@ -8,39 +9,71 @@ function errorOnPage(message, response){
     response.end('Not Found');
 }
 
+function makeFileList(files){
+    let list = '<ul>';
+
+    for(let i = 0; i < files.length; i++){
+        if(files[i] == 'index.html')
+            continue;
+        list = list + `<li><a href="/?id=${files[i]}&type=html">${files[i]}</a></li>`
+    }
+
+    list = list+'</ul>';
+    return list;
+}
+
 let app = http.createServer(function(request,response){
     let _url = request.url;
-    let status;
-    // home: index로 지정
+    var queryData = url.parse(_url, true).query;
+    console.log(queryData);
     if(_url == '/'){
-        status = 'index';
+        // home: index로 지정
+        _url = '/index.html';
+        queryData.type = 'html';
     }
-    else {
-        let query = url.parse(_url, true).query;
-        status = query.id;
+    else if(queryData.type == 'html'){
+        _url = '/' + queryData.id;
     }
-    status += '.html';
+    else if(queryData.type == 'description'){
+        _url = '/description.html';
+    }
+    else if(queryData.type == 'passing'){
+        let body = '';
+        request.on('data',(data)=>{
+            body = body + data;
+        });
+        request.on('end',()=>{
+            let post = qs.parse(body);
+            let title = post.title;
+            let description = post.description;
+            fs.writeFile(`data/${title}`, description, 'utf8',(err)=>{
+                if(err){
+                    errorOnPage("File Creating Error",response);
+                }
+                response.writeHead(302,{Location: `/?id=${title}`});
+                response.end('success');
+            });
+        });
+    }
+    _url = '/html' + _url;
 
-    // 파일 포함 여부 확인
-    fs.readdir('html',(err, files)=>{
-        if(err){
-            errorOnPage("Connection Failed",response);
-        }
-
-        if(files.indexOf(status)<0){
-            errorOnPage("No Existing File",response);
-        }
-    })
-
-    _url = `/html/${status}`;
-    fs.readFile(__dirname+_url, function(err,data){
-        if(err){
-            errorOnPage("file loading error",response);
-        }
-        else{
-            response.writeHead(200, {'Content-Type':'text/html'});
-            response.end(data);
-        }
-    });
+    // 파일 읽어서 리스트 만들기
+    if(queryData.type == 'html'){
+        fs.readdir('html',(err, files)=>{
+            if(err){
+                errorOnPage("Connection Failed",response);
+            }
+            let list = makeFileList(files);
+    
+            fs.readFile(__dirname+_url, 'utf8',function(err,data){
+                if(err){
+                    errorOnPage(`file loading error!\nrequest_url: ${_url}`,response);
+                }
+                data = data.replace('{list}',list);
+                response.writeHead(200, {'Content-Type':'text/html'});
+                response.end(data);
+            });
+        });
+    }
 })
 .listen(3000);
